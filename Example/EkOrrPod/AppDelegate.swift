1ae84a7b22b3c2fce7aee03f766be52423fe1340
ajha <ajha@cloudsmith.io>
2025-07-24 23:11:42 +0530

    Initial commit

---
+++ Example/EkOrrPod/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EkOrrPod
+//
+//  Created by 201456038 on 07/24/2025.
+//  Copyright (c) 2025 201456038. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Example/EkOrrPod/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EkOrrPod
+//
+//  Created by 201456038 on 07/24/2025.
+//  Copyright (c) 2025 201456038. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
